a47e6a07c7c84cb484f6332c5baf3c981f9915a7
harshithva <harshith11032001@gmail.com>
2022-10-21 11:25:00 +0530

    project dockerized

--- myworld/members/admin.py
+++ myworld/members/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Students
+from .models import Members
 
-class DjStudentAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "address", "roll_number", "mobile", "branch")
-    list_filter = ("branch",)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname')
 
+admin.site.register(Members,PersonAdmin)
 # Register your models here.
-admin.site.register(Students, DjStudentAdmin)
